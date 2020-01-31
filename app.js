55fe65ca93338b5e98608e77b9160e8bfc30c5d9
SasanoHayato <sasano.hayato@gmail.com>
2020-01-31 12:54:00 +0800

    adjust order

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
+// イメージアニメーション
+$(function(){
+  var $setElm = $('.viewer'),
+  fadeSpeed = 5000,
+  switchDelay = 8000;
+
+  $setElm.each(function(){
+      var targetObj = $(this);
+      var findUl = targetObj.find('ul');
+      var findLi = targetObj.find('li');
+      var findLiFirst = targetObj.find('li:first');
+
+      findLi.css({display:'block',opacity:'0',zIndex:'99'});
+      findLiFirst.css({zIndex:'100'}).stop().animate({opacity:'1'},fadeSpeed);
+
+      setInterval(function(){
+          findUl.find('li:first-child').animate({opacity:'0'},fadeSpeed).next('li').css({zIndex:'100'}).animate({opacity:'1'},fadeSpeed).end().appendTo(findUl).css({zIndex:'99'});
+      },switchDelay);
+  });
+});
+
 //scroll up
 $('.icon').on('mouseover', function () {
   $('body, html').animate({ scrollTop: 0 }, 500);
@@ -20,7 +41,7 @@ $('.left_header').on('mouseleave', function() {
 });
 
 
-
+// modal window
 $(function() {
   //モーダルを開く
   $('.modalBtn').click(function() {
@@ -33,22 +54,3 @@ $(function() {
 });
 
 
-$(function(){
-  var $setElm = $('.viewer'),
-  fadeSpeed = 1500,
-  switchDelay = 5000;
-
-  $setElm.each(function(){
-      var targetObj = $(this);
-      var findUl = targetObj.find('ul');
-      var findLi = targetObj.find('li');
-      var findLiFirst = targetObj.find('li:first');
-
-      findLi.css({display:'block',opacity:'0',zIndex:'99'});
-      findLiFirst.css({zIndex:'100'}).stop().animate({opacity:'1'},fadeSpeed);
-
-      setInterval(function(){
-          findUl.find('li:first-child').animate({opacity:'0'},fadeSpeed).next('li').css({zIndex:'100'}).animate({opacity:'1'},fadeSpeed).end().appendTo(findUl).css({zIndex:'99'});
-      },switchDelay);
-  });
-});
